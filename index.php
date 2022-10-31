978dcc8e4f9f32b92ba08e15e3a69e58494fea22
Arthur Chiappetta <arthurchiappetta@gmail.com>
2022-10-31 10:17:48 -0300

    tag script removida e implementado form p pesquisa

--- index.php
+++ index.php
@@ -8,8 +8,8 @@ $cadastro = new CadastroAluno($mysql);
 $cadastros = $cadastro->exibirTodos();
 
 $buscaEspecifica = new BuscaEspecifica($mysql);
-if (isset($_GET['search'])) {
-  $busca = $buscaEspecifica->buscaEspecifica($_GET['search']);
+if (isset($_GET['busca'])) {
+  $busca = $buscaEspecifica->buscaEspecifica($_GET['busca']);
 }
 ?>
 
@@ -32,16 +32,10 @@ if (isset($_GET['search'])) {
     <h1>Alunos Cadastrados</h1>
   </center>
 
-
-  <div class="box-serch">
-    <input type="search" class="form-control w-25" placeholder="Pesquisar" id="pesquisar">
-    <button onclick="searchData()">
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-      </svg>
-    </button>
-  </div>
-
+  <form action="">
+    <input name="busca" placeholder="Pesquisar" type="text">
+    <button type="submit">Pesquisar</button>
+  </form>
   <br />
   <table class="table" border="1">
     <thead>
@@ -52,7 +46,7 @@ if (isset($_GET['search'])) {
       </tr>
     </thead>
     <tbody>
-      <?php if (isset($_GET['search'])) {
+      <?php if (isset($_GET['busca'])) {
         foreach ($busca as $buscaEspecifica) : ?>
           <tr>
             <td scope="row"><?php echo $buscaEspecifica['id'] ?></td>
@@ -73,33 +67,10 @@ if (isset($_GET['search'])) {
     </tbody>
   </table>
 
-
-  <!-- Optional JavaScript -->
-  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
-  <br />
-
-  <div>
-    <center>
-      <a href="admin/index.php"><button>Pagina Administrativa</button></a>
-    </center>
+  <center>
+    <a href="admin/index.php"><button>Pagina Administrativa</button></a>
+  </center>
   </div>
 </body>
-<script>
-  var search = document.getElementById('pesquisar');
-
-  search.addEventListener("keydown", function(event) {
-    if (event.key === "Enter") {
-      searchData();
-    }
-  });
-
-  function searchData() {
-    window.location = 'index.php?search=' + search.value;
-  }
-</script>
 
 </html>
